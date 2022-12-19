0fc292e05d533f5d9a6bb46a8c5e79018948e07d
Dhayanshariff <dhayanshariff92@gmail.com>
2022-12-19 08:46:18 +0530

    Delete Semantic segmentation_image.py

--- Semantic segmentation_image.py
+++
@@ -1,7 +0,0 @@
-import pixellib
-import cv2
-from pixellib.semantic import semantic_segmentation
-
-seg_img = semantic_segmentation()
-seg_img.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
-seg_img.segmentAsPascalvoc("Image.jpg", output_image_name = "output_image.jpg") # Change the input filename accordingly
